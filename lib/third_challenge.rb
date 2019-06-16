b9cc57641a7d10734240783ed4fbddbc474e96e7
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-16 19:25:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,8 +24,10 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine => {:name => "Juliet", :age => "15", :status => "alive"
-        :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}]
+        :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+          ]
    }
 }
   
