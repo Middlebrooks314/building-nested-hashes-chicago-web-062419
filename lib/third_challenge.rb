2ea3e435a5d3669acc523ddfa660b3b864a9d50e
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-16 19:47:24 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,9 +23,11 @@ def third_challenge
       :matriarch => {:name => "Lady Capulet", :age => "51"
         #your key/value pairs here
         },
-      :heroine => {:name => "Juliet", :age => "15", :status => "alive"
-        :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}]
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
+        :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+          ]
    }
 }
   
